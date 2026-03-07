81482fcfe22370f9b10fb2d996010f07b4254b3e
Alex Gonzalez <alxgnz@MacBook-Pro.local>
2026-03-07 14:01:55 -0600

    updated cousins site layout and images

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,15 @@
+import logo from "../assets/images/hero/cousins-pizza-logo.png";
+
 export default function Header() {
     return (
-        <header className="sticky top-0 z-50 bg-cream border-b border-black/10">
+        <header className="sticky top-0 z-50 bg-[#5A1E1E] border-b border-black/10">
+
             <div className="max-w-6xl mx-auto px-6 h-16 flex items-center justify-between">
-                <div className="font-serif text-xl tracking-wide">
-                    Cousins Pizza Pub
-                </div>
+                <img
+                    src={logo}
+                    alt="Cousins Pizza Pub"
+                    className="h-18 md:h-20 w-auto"
+                />
 
                 <nav className="hidden md:flex gap-8 text-sm uppercase tracking-widest">
                     <a href="#menu" className="hover:text-pubRed">Menu</a>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,22 +1,32 @@
 import heroImage from "../assets/images/hero/cousins-outdoor-img.jpg";
+import logo from "../assets/images/hero/cousins-pizza-logo.png"
 
 export default function Hero() {
   return (
     <section className="relative h-[60vh] md:h-[75vh] min-h-[500px] w-screen overflow-hidden">
       <div
         className="absolute inset-0 bg-cover bg-[position:center_35%]"
-        style={{ backgroundImage: `url(${heroImage})` }}
+      //style={{ backgroundImage: `url(${heroImage})` }}
       />
 
       <div className="absolute inset-0 bg-black/55" />
 
-      <div className="relative z-10 flex h-full w-full items-center justify-center pt-24">
+      <div className="relative z-10 flex h-full w-full items-center justify-center pt-16 md:pt-20">
         <div className="mx-auto max-w-3xl px-6 text-center">
-          <h1 className="text-5xl md:text-6xl font-serif text-cream">
-            Welcome to Cousins
+          <h1 className="text-4xl md:text-5xl font-serif text-cream">
+            Welcome to
           </h1>
 
-          <p className="mt-6 text-lg md:text-xl text-cream/80">
+          <div className="mt-3 flex justify-center">
+            <img
+              src={logo}
+              alt="Cousins Pizza Pub"
+              className="h-52 md:h-54 lg:h-52 w-auto"
+
+            />
+          </div>
+
+          <p className="mt-4 text-lg md:text-xl text-cream/80">
             A neighborhood pub serving classic pizza, cold beer,
             and familiar faces — just the way it’s always been.
           </p>
